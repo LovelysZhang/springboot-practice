80be9c06c38c1270e87aea46172019fb8fe8b021
LovlysZhang <3161422489@qq.com>
2019-11-17 01:13:50 +0800

    qmq

---
+++ springboot-qmq-provider/src/main/java/com/qmq/QmqProviderApplication.java
@@ -0,0 +1,15 @@
+package com.qmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author non human
+ * on 2019/11/13
+ */
+@SpringBootApplication
+public class QmqProviderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QmqProviderApplication.class);
+    }
+}
