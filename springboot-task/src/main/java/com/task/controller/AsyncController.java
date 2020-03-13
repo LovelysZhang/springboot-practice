50c44da18c840db1f26c4c9dc171fd39cfbd0e2b
zhanghaojie.zhang <zhanghaojie.zhang@qunar.com>
2020-03-14 04:35:39 +0800

    springboot+docker

--- springboot-task/src/main/java/com/task/controller/AsyncController.java
+++ springboot-task/src/main/java/com/task/controller/AsyncController.java
@@ -3,10 +3,16 @@ package com.task.controller;
 import com.task.service.AsyncService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 异步任务
+ *
  * @author non human
  * on 2020/2/12
  */
@@ -19,5 +25,17 @@ public class AsyncController {
     @GetMapping("/say")
     public void say() {
         asyncService.asyncHello();
+    }
+
+    @GetMapping("/get/{id}")
+    @ResponseBody
+    public List<Data> getData(@PathVariable String id) {
+        List<Data> list = new ArrayList<>();
+        return "0".equals(id) ? null : list;
+    }
+
+    class Data {
+        Integer id;
+        String name;
     }
 }
